e0b651869326505c7b358d4088848ecc7633aafe
TriLau <trilau78952@gmail.com>
2025-02-18 23:57:38 +0700

    fixing ride bike

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,24 +6,37 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 1f;
     public float runSpeed = 1f;
-    public float bicycleSpeed = 1f;
-    private float currentSpeed;
+    public float vehicleSpeed;
+
+    [SerializeField]
+    private float _currentSpeed;
+    public float CurrentSpeed
+    {
+        get 
+        {
+            return _currentSpeed = IsRidingVehicle ? vehicleSpeed : IsRuning ? runSpeed : walkSpeed;
+        }
+    }
+
     private Vector2 movement;
     private Vector2 lastMovement;
-    public Vector2 Movement 
+    public Vector2 Movement
     {
         get { return lastMovement; }
     }
+
     private Rigidbody2D rb;
     private Animator animator;
+    private Collider2D col;
+    private VehicleController curentVehicle;
 
     [SerializeField]
     private bool _isFacingRight = true;
     public bool IsFacingRight
     {
         get { return _isFacingRight; }
-        set 
-        { 
+        set
+        {
             if (_isFacingRight != value)
             {
                 _isFacingRight = value;
@@ -33,25 +46,38 @@ public class PlayerController : MonoBehaviour
     }
 
     [SerializeField]
-    private bool _isUsingBicycle = false;
-    public bool IsUsingBicycle
+    private bool _canRun = true;
+    public bool CanRun
     {
-        get { return _isUsingBicycle; }
-        private set
-        {
-            _isUsingBicycle = value;
-            animator.SetBool("UseDevice", value);
-        }
+        get { return _canRun; }
+        private set { _canRun = value; }
     }
 
     [SerializeField]
-    private bool canRide = false;
+    private bool _isRuning = false;
+    public bool IsRuning
+    {
+        get { return _isRuning; }
+        private set { _isRuning = value; }
+    }
+
+    [SerializeField]
+    private bool _canRide = false;
     public bool CanRide
     {
-        get { return canRide;}
-        set
+        get { return _canRide; }
+        private set { _canRide = value; }
+    }
+
+    [SerializeField]
+    private bool _isRidingVehicle = false;
+    public bool IsRidingVehicle
+    {
+        get { return _isRidingVehicle; }
+        private set
         {
-            canRide = value;
+            _isRidingVehicle = value;
+            animator.SetBool("UseDevice", value);
         }
     }
 
@@ -59,11 +85,13 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : IsUsingBicycle ? bicycleSpeed : walkSpeed;
+        if (CanRun && Input.GetKey(KeyCode.LeftShift)) IsRuning = true;
+        else IsRuning = false;
 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
@@ -80,15 +108,59 @@ public class PlayerController : MonoBehaviour
 
         if (movement.x > 0 && !IsFacingRight) IsFacingRight = true;
         else if (movement.x < 0 && IsFacingRight) IsFacingRight = false;
-        
+
         if (Input.GetKeyDown(KeyCode.E) && CanRide)
         {
-            IsUsingBicycle = !IsUsingBicycle;
+            IsRidingVehicle = !IsRidingVehicle;
+
+            if (IsRidingVehicle)
+            {
+                transform.SetParent(curentVehicle.transform);
+                curentVehicle.SetRiding(true);
+            }
+            else
+            {
+                transform.SetParent(null);
+                curentVehicle.SetRiding(false);
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * currentSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * CurrentSpeed * Time.fixedDeltaTime);
+    }
+
+    public void SetCurrentVehicle(VehicleController vehicle)
+    {
+        if (IsRidingVehicle) return;
+
+        CanRide = true;
+        curentVehicle = vehicle;
+    }
+
+    public void ClearVehicle()
+    {
+        if (!IsRidingVehicle)
+        {
+            CanRide = false;
+            curentVehicle = null;
+        }
+    }
+
+    public void OnMove(Vector2 move)
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(moveX, moveY).normalized;
+
+        if (movement != Vector2.zero) lastMovement = movement;
+
+        animator.SetFloat("Horizontal", Mathf.Abs(lastMovement.x));
+        animator.SetFloat("Vertical", lastMovement.y);
+
+        animator.SetFloat("Speed", movement.magnitude);
+        animator.SetBool("IsRunning", Input.GetKey(KeyCode.LeftShift));
     }
 }
