fc3bc7dcebe01badb1fbde48d21a18ba3269d2d2
thau7777 <thau7777@gmail.com>
2025-02-19 20:22:09 +0700

    Revert "add cinemachine and horse"
    This reverts commit c0579a7a27cda979c994c676afe48dbc7710bc3c.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,26 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 1f;
-    public float runSpeed = 1f;
-
-    [SerializeField]
-    private float _currentSpeed;
-    public float CurrentSpeed
-    {
-        get 
-        {
-            return _currentSpeed = IsRuning ? runSpeed : walkSpeed;
-        }
-    }
-
-    private Vector2 movement;
-    private Vector2 lastMovement;
-    public Vector2 LastMovement
-    {
-        get { return lastMovement; }
-    }
-
+    private Player player;
     private Rigidbody2D rb;
     private Animator animator;
     private Collider2D col;
@@ -83,6 +64,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        player = GetComponent<Player>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         col = GetComponent<Collider2D>();
@@ -121,7 +103,7 @@ public class PlayerController : MonoBehaviour
     {
         if (!IsRidingVehicle)
         {
-            rb.MovePosition(rb.position + movement * CurrentSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + player.Movement * player.CurrentSpeed * Time.fixedDeltaTime);
         }
     }
 
@@ -152,7 +134,7 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        movement = new Vector2(moveX, moveY).normalized;
+        player.Movement = new Vector2(moveX, moveY).normalized;
 
         if (movement != Vector2.zero) lastMovement = movement;
 
