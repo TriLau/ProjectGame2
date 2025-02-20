cafdc85d41b4d0903c75ea505e682f352888e489
TriLau <trilau78952@gmail.com>
2025-02-20 20:26:25 +0700

    update inventory

---
+++ Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Scripable object/Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Only gameplay")]
+    public TileBase tile;
+    public ItemType type;
+    public ActionType actionType;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+    [Header("Only UI")]
+    public bool stackable = true;
+
+    [Header("Both")]
+    public Sprite image;
+    public string itemName;
+}
+
+public enum ItemType
+{
+    Tile,
+    Food,
+    Tool
+}
+
+public enum ActionType
+{
+    Cook,
+    Combine,
+    Mine
+}
