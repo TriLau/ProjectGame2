126c30c9bf04534efcec5b3dd108903fc3612b97
TriLau <trilau78952@gmail.com>
2025-02-21 15:55:52 +0700

    update environmental resources

---
+++ Assets/Scripts/Enviroment/EnvironmentalResource.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum ERState
+{
+    Full,
+    Half,
+    Origin
+}
+
+public class EnvironmentalResource : MonoBehaviour
+{
+    
+}
