ea1070e68d7aa04e422b9ac936be998778807fce
Ridotakarin <ridotakarin@gmail.com>
2025-04-02 20:24:56 +0700

    them chu thich

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,14 +9,15 @@ public class Attack : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Damageable damageable = collision.gameObject.GetComponent<Damageable>();
+        Damageable damageable = collision.gameObject.GetComponent<Damageable>();// Find object damageable 
 
         if (damageable != null)
         {
-            float direction = Mathf.Sign(collision.transform.position.x - transform.position.x);
-            Vector2 deliveredKnockBack = new Vector2(knockback.x * direction, knockback.y);
+            float direction = Mathf.Sign(collision.transform.position.x - transform.position.x); // Math new direction 
+            Vector2 deliveredKnockBack = new Vector2(knockback.x * direction, knockback.y);// Create a new position
 
-            bool goHit = damageable.Hit(attackDamage, deliveredKnockBack);
+            bool goHit = damageable.Hit(attackDamage, deliveredKnockBack); // Update damage and new position
+            
 
             if (goHit)
             {
